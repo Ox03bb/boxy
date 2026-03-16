2e108edc61fcb94367271c797d0f1d9658b8570f
ox03bb <alibouakaz91@gmail.com>
2026-03-16 20:44:02 +0100

    hardcode the home path for dev

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,9 @@ package config
 const (
 	SocketPath = "/tmp/boxyd.sock"
 
-	ProjectPath = "$HOME/Desktop/boxy"
+	HOME = "/home/ox03bb" //"$HOME"
+
+	ProjectPath = HOME + "/Desktop/boxy"
 
 	RegistryPath = ProjectPath + "/registry"
 	EnvPath      = ProjectPath + "/env"
