7a084e1a99bb82a739fe4a60884d7759c28f98f9
ox03bb <alibouakaz91@gmail.com>
2026-03-17 14:19:58 +0100

    refactor: simplify run command by moving IPC handling to RunAndAttach function

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,16 +1,11 @@
 package cli
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net"
 	"os"
 
 	"github.com/Ox03bb/boxy/internal/cli/handler"
-	"github.com/Ox03bb/boxy/internal/ipc"
 	"github.com/spf13/cobra"
-	"golang.org/x/term"
 )
 
 var rootCmd = &cobra.Command{
@@ -24,65 +19,16 @@ var runCmd = &cobra.Command{
 	Use:   "run [OPTIONS] IMAGE [COMMAND]",
 	Short: "Run the boxy command",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		req, err := handler.RunHandler(cmd, args)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
-		sock, err := ipc.Connect("")
-		if err != nil {
-			fmt.Println("Error connecting to daemon:", err)
+		if err := handler.RunAndAttach(req); err != nil {
+			fmt.Println("Error:", err)
 			return
 		}
-		defer ipc.Close(sock)
-
-		reqBytes, err := json.Marshal(req)
-		if err != nil {
-			fmt.Println("Error marshaling request:", err)
-			return
-		}
-
-		if err := ipc.Send(sock, reqBytes); err != nil {
-			fmt.Println("Error sending request to daemon:", err)
-			return
-		}
-
-		unixSock, ok := sock.(*net.UnixConn)
-		if !ok {
-			fmt.Println("Error: socket is not a unix domain socket")
-			return
-		}
-
-		fd, err := ipc.ReceiveFD(unixSock)
-		if err != nil {
-			fmt.Println("Error receiving FD:", err)
-			return
-		}
-
-		ptyFile := os.NewFile(uintptr(fd), "pty")
-		if ptyFile == nil {
-			fmt.Println("Error: failed to create file from fd")
-			return
-		}
-		defer ptyFile.Close()
-
-		// Put the user's terminal into raw mode
-		oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-		if err != nil {
-			fmt.Println("Error setting raw mode:", err)
-			return
-		}
-		defer term.Restore(int(os.Stdin.Fd()), oldState)
-
-		// Forward user input to the PTY
-		go func() {
-			_, _ = io.Copy(ptyFile, os.Stdin)
-		}()
-
-		// Forward PTY output to the user terminal
-		_, _ = io.Copy(os.Stdout, ptyFile)
 	},
 }
 
